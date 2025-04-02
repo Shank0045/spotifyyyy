c36a5a4e9424e6f2cbe86bd8832fecd9e024cfa8
shashank <shashank10733@gmail.com>
2025-04-02 19:54:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,52 @@
 let currentsong = new Audio();
 
 let songs;
+let currentSongIndex = 0;
 
-let currentSongIndex=0
-
-currentsong.volume=0.01;
+currentsong.volume = 0.01;
 
 function convertToMMSS(seconds) {
   const minutes = Math.floor(seconds / 60);
-
-
-  const remainingSeconds = Math.floor(seconds % 60); 
-
-
+  const remainingSeconds = Math.floor(seconds % 60);
   const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
   const formattedSeconds =
     remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
 
-
   return `${formattedMinutes}:${formattedSeconds}`;
 }
 
+// Update the getsong function
 async function getsong() {
-  let responce = await fetch("https://raw.githubusercontent.com/Shank0045/spotifyyyy/main/songs/");
+  // Manually list the songs in your 'songs' folder here, or automate it via GitHub API (this is a simplified version)
+  const songFiles = [
+    "song1.mp3", // Replace with your actual filenames
+    "song2.mp3", // Replace with your actual filenames
+    "song3.mp3"  // Replace with your actual filenames
+  ];
 
-  let data = await responce.text();
-
-  let div = document.createElement("div");
-  div.innerHTML = data;
-  let a = div.getElementsByTagName("a");
+  const baseURL = "https://raw.githubusercontent.com/Shank0045/spotifyyyy/main/songs/";
 
   let sogul = document.querySelector(".addmusic").getElementsByTagName("ul")[0];
 
-  for (let song of a) {
-    if (song.href.endsWith(".mp3")) {
-      sogul.innerHTML =
-        sogul.innerHTML +
-        `  <li>
-                            <div class="music"> <img class="invert"  src="music.svg" alt="music">music</div>
-                            <div class="songname ">${song.href
-                              .split("/songs/")[1]
-                              .replaceAll("%20", " ")}</div>
-                            
-                        </li>`;
-    }
-  }
+  // Loop through song files and add them to the list
+  songFiles.forEach((song) => {
+    let songName = song.replaceAll("%20", " "); // To handle spaces in file names
+    sogul.innerHTML += `
+      <li>
+        <div class="music"><img class="invert" src="music.svg" alt="music"> music</div>
+        <div class="songname">${songName}</div>
+      </li>`;
+  });
 
-  let songs = [];
-
-  for (let song of a) {
-    if (song.href.endsWith(".mp3")) {
-      songs.push(song.href);
-    }
-  }
-
-  return songs;
+  // Return the full URLs of the songs
+  return songFiles.map((song) => baseURL + song);
 }
 
 const playmusic = (track, pause = false) => {
-  currentsong.src = "/songs/" + track;
-  console.log(track)
+  // Play the song from the GitHub raw URL
+  currentsong.src = "https://raw.githubusercontent.com/Shank0045/spotifyyyy/main/songs/" + track;
+  console.log(track);
+
   if (!pause) {
     currentsong.play();
     play.src = "pause.svg";
@@ -70,19 +57,20 @@ const playmusic = (track, pause = false) => {
 };
 
 async function main() {
-  songs = await getsong();
+  songs = await getsong(); // Fetch the song list
+
+  // Play the first song initially
   playmusic(songs[0].split("/songs/")[1], true);
 
-  Array.from(
-    document.querySelector(".addmusic").getElementsByTagName("li")
-  ).forEach((e) => {
+  // Set up click events for each song in the list
+  Array.from(document.querySelector(".addmusic").getElementsByTagName("li")).forEach((e) => {
     e.addEventListener("click", () => {
       console.log(e.querySelector(".songname").innerHTML);
-
       playmusic(e.querySelector(".songname").innerHTML);
     });
   });
 
+  // Set up the play/pause button
   play.addEventListener("click", () => {
     if (currentsong.paused) {
       currentsong.play();
@@ -91,11 +79,13 @@ async function main() {
       currentsong.pause();
       play.src = "play.svg";
     }
-    vol.addEventListener("change",(e)=>{
-      currentsong.volume=parseInt(e.target.value)/100 
-  })
+
+    vol.addEventListener("change", (e) => {
+      currentsong.volume = parseInt(e.target.value) / 100;
+    });
   });
 
+  // Update song time and position in the progress bar
   currentsong.addEventListener("timeupdate", () => {
     document.querySelector(".songtime").innerHTML = `${convertToMMSS(
       currentsong.currentTime
@@ -105,45 +95,36 @@ async function main() {
       (currentsong.currentTime / currentsong.duration) * 100 + "%";
   });
 
+  // Seekbar functionality
   document.querySelector(".seekbar").addEventListener("click", (e) => {
     let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
     document.querySelector(".circle").style.left = percent + "%";
-
     currentsong.currentTime = (currentsong.duration * percent) / 100;
   });
 
-
-
+  // Playlist slide-in/out
   document.querySelector(".playlist").addEventListener("click", () => {
-  document.querySelector(".lmain").style.left = 0+"%";
-});
-
-
-document.querySelector(".wrong").addEventListener("click", () => {
-  document.querySelector(".lmain").style.left = -100 + "%";
-});
-
-
-
-  document.getElementById("next").addEventListener("click", () => {
-    currentSongIndex = (currentSongIndex + 1) % songs.length; 
-    playmusic(songs[currentSongIndex].split("/songs/")[1]);
+    document.querySelector(".lmain").style.left = 0 + "%";
   });
 
+  document.querySelector(".wrong").addEventListener("click", () => {
+    document.querySelector(".lmain").style.left = -100 + "%";
+  });
+
+  // Next/Previous buttons functionality
+  document.getElementById("next").addEventListener("click", () => {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
+    playmusic(songs[currentSongIndex].split("/songs/")[1]);
+  });
 
   document.getElementById("previous").addEventListener("click", () => {
-    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length; 
+    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
     playmusic(songs[currentSongIndex].split("/songs/")[1]);
   });
 
-
-  vol.addEventListener("change",(e)=>{
-      currentsong.volume=parseInt(e.target.value)/100 
-  })
-
-
-
+  vol.addEventListener("change", (e) => {
+    currentsong.volume = parseInt(e.target.value) / 100;
+  });
 }
 
 main();
-
